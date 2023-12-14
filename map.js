d8129cdaef694406e1678d534b8e4f0398e02fbf
roc-11 <>
2023-12-14 10:56:45 +0000

    Practice using the .map() method

--- map.js
+++ map.js
@@ -6,15 +6,35 @@
 
 // Using a for loop
 
+let nums = [1, 2, 3, 4, 5];
+let results = [];
+
+for(let num of nums) {
+    results.push(num * 2);
+}
+
+console.log(results);
+
 
 // Using map()
 
+const multiplyByTwo = function(num) {
+    return num * 2;
+}
+
+const mapResults = nums.map(multiplyByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
+const mapResult2 = nums.map(function(num) {
+    return num * 2;
+});
+console.log(mapResult2);
 
 
 // Simplfied w/ map() + arrow function
-
+const mapResultsSimp = nums.map(num => num * 2);
+console.log(mapResultsSimp);
 
 // With objects:
 const students = [
@@ -37,3 +57,6 @@ const students = [
     skill: 'CSS'
   },
 ];
+
+const studentsWithIds = students.map((student) => [student.name, student.id]);
+console.log(studentsWithIds);
